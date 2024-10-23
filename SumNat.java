9f2123dedd0ebdfa1ab771df47d41c787bd52b0c
mhklmao <mhkjune2004@gmail.com>
2024-10-22 21:53:49 -0700

    Update SumNat.java

--- SumNat.java
+++ SumNat.java
@@ -1,16 +1,24 @@
-/* Q : Sum of first 10 Natural Numbers */
+/* Q : Sum of first n Natural Numbers */
 
 package com.mycompany.sumnat;
+
+import java.util.Scanner;
 
 public class SumNat {
 
     public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter n : ");
+        int n = scanner.nextInt();
+        
         int sum = 0;
         
-        for (int i = 1; i < 11; i++) {
+        for (int i = 1; i <= n; i++) {
             sum = sum + i;
         }
         
-        System.out.println("The sum of first 10 Natural numbers is : " + sum);
+        System.out.println("The sum of first " +n+ " Natural numbers is : " + sum);
     }
 }
