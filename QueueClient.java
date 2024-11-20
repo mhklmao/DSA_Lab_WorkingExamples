8af1b4a9621720978cbba3b59b1131ddcc2ca320
mhklmao <mhkjune2004@gmail.com>
2024-11-19 21:26:36 -0800

    Add files via upload

---
+++ QueueClient.java
@@ -0,0 +1,90 @@
+
+package Lab07;
+
+class LinkedList {
+    private Node front;
+    private Node rear;
+
+    static class Node {
+        public int data;
+        public Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public boolean isEmpty() {
+        return (front == null);
+    }
+
+    public void insertLast(int new_data) {
+        Node new_node = new Node(new_data);
+        if (isEmpty()) {
+            front = new_node;
+        } else {
+            rear.next = new_node;
+        }
+        rear = new_node;
+    }
+    
+    public int deleteFirst() {
+        int temp = front.data;
+
+        if (front.next == null) {
+            rear = null;
+        }
+
+        front = front.next;
+        return temp;
+    }
+
+    void displayList() {
+        System.out.println("===Displaying Elements of DEQ===");
+        Node current = front;
+
+        while (current != null) {
+            System.out.println(current.data);
+            current = current.next;
+        }
+    }
+}
+
+class Queue2 {
+    LinkedList llist;
+
+    public Queue2() {
+        llist = new LinkedList();
+    }
+
+    public boolean isEmpty() {
+        return llist.isEmpty();
+    }
+
+    public void enqueue(int new_data) {
+        llist.insertLast(new_data);
+    }
+
+    public int dequeue() {
+        return llist.deleteFirst();
+    }
+
+    void printQueue() {
+        llist.displayList();
+    }
+}
+
+public class QueueClient {
+    public static void main(String[] args) {
+
+        Queue2 list = new Queue2();
+        list.enqueue(10);
+        list.enqueue(20);
+        list.enqueue(30);
+        list.enqueue(40);
+        list.printQueue();
+        list.dequeue();
+        list.printQueue();
+    }
+}
